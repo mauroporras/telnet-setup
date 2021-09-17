7d1d31f51e658647e022410de4ff0b89697c6a4f
mauroporras <167700+mauroporras@users.noreply.github.com>
2021-09-17 11:35:38 -0500

    feat: add Express and CORS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,44 +1,44 @@
-import * as functions from "firebase-functions";
+import * as cors from "cors";
+import * as express from "express";
 import * as firebase from "firebase-admin";
+import * as functions from "firebase-functions";
 
 firebase.initializeApp();
 
 const db = firebase.firestore();
+
+const app = express();
 
 interface PostRequest {
   sessionId?: string;
   latestSelectedAnchor?: string;
 }
 
-const setLatestSelectedAnchor = functions.https.onRequest(
-  async (request, response) => {
-    const isPost = request.method.toUpperCase() === "POST";
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
 
-    if (!isPost) {
-      response.send(405);
-      return;
-    }
+app.post("/", async (request, response) => {
+  const { sessionId, latestSelectedAnchor } = request.body as PostRequest;
 
-    const { sessionId, latestSelectedAnchor } = request.body as PostRequest;
-
-    if (!sessionId || !latestSelectedAnchor) {
-      response.send(400);
-      return;
-    }
-
-    const collectionId = "sessions";
-    const docRef = db.collection(collectionId).doc(sessionId);
-    const doc = await docRef.get();
-
-    if (!doc.exists) {
-      response.send(404);
-      return;
-    }
-
-    await docRef.update({ latestSelectedAnchor });
-
-    response.send({ sessionId, latestSelectedAnchor });
+  if (!sessionId || !latestSelectedAnchor) {
+    response.send(400);
+    return;
   }
-);
+
+  const collectionId = "sessions";
+  const docRef = db.collection(collectionId).doc(sessionId);
+  const doc = await docRef.get();
+
+  if (!doc.exists) {
+    response.send(404);
+    return;
+  }
+
+  await docRef.update({ latestSelectedAnchor });
+
+  response.send({ sessionId, latestSelectedAnchor });
+});
+
+const setLatestSelectedAnchor = functions.https.onRequest(app);
 
 export { setLatestSelectedAnchor };
