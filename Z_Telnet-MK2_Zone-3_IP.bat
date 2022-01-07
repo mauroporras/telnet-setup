747b37e68a01444b729e532d40b6ea84bb71bbe2
Nathan Barnes <nbarnes@azahner.com>
2022-01-07 09:08:56 -0500

    Update Batch

--- Z_Telnet-MK2_Zone-3_IP.bat
+++ Z_Telnet-MK2_Zone-3_IP.bat
@@ -1,3 +1,3 @@
 cd "%UserProfile%\Documents\GitHub\telnet-setup"
-set ZEA_STREAMER_TYPE=telnet&&set ZEA_SESSION_ID=sector-zone-3&&set ZEA_TELNET_HOST=192.168.1.2&&call yarn start
+set ZEA_STREAMER_TYPE=telnet&&set ZEA_SESSION_ID=sector-zone-3-1&&set ZEA_TELNET_HOST=192.168.1.2&&call yarn start
 pause
---
+++ Z_Telnet-MK2_Zone-3_IP_Shift.bat
@@ -0,0 +1,3 @@
+cd "%UserProfile%\Documents\GitHub\telnet-setup"
+set ZEA_STREAMER_TYPE=telnet&&set ZEA_SESSION_ID=sector-zone-3_Shift&&set ZEA_TELNET_HOST=192.168.1.5&&call yarn start
+pause
