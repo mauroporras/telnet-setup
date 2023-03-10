9ba2d049c24cf1da1de0985fbbfcde97aeaaca77
mauroporras <167700+mauroporras@users.noreply.github.com>
2023-03-10 16:46:20 -0500

    feat: add commands helper

--- src/models/Command.js
+++ src/models/Command.js
@@ -1,5 +1,13 @@
 import { db } from '../helpers/firebase.js'
 
+const TotalStationCommands = {
+  turnTelescope: (x, y, z) => `%R8Q,7:1,${x},${y},${z}\r\n`,
+  START_STREAM: '%R8Q,4:\r\n',
+  SAMPLE_DIST: '%R8Q,1:\r\n',
+  SEARCH: '%R8Q,6:1\r\n',
+  STOP_STREAM: '%R8Q,5:\r\n',
+}
+
 const TotalStationErrors = {
   GRC_OK: 0,
   // GRC_NOT_OK:40,
@@ -23,24 +31,22 @@ class Command {
 
   async invoke() {
     return new Promise(async (resolve) => {
-      let result
-
       const { x, y, z } = this.data.position
-      result = await this.streamer.send(`%R8Q,7:1,${x},${y},${z}\r\n`)
-      result = await this.streamer.send('%R8Q,1:\r\n')
-      result = await this.streamer.send('%R8Q,6:1\r\n')
-      result = await this.streamer.send('%R8Q,4:\r\n')
 
-      // Wait for the return code....
+      this.streamer.send(TotalStationCommands.turnTelescope(x, y, z))
+      this.streamer.send(TotalStationCommands.START_STREAM)
+      this.streamer.send(TotalStationCommands.SAMPLE_DIST)
+      this.streamer.send(TotalStationCommands.SEARCH)
+
       const off = this.streamer.on('data', async (point) => {
+        off()
+
+        this.streamer.send(TotalStationCommands.STOP_STREAM)
+
         await this.#markAsInvoked()
 
         // TS0012,410.9147,512.9075,103.3155,10/07/2020,18:53:02.68,16934825
         await this.session.addPoint(point, this.data.anchor)
-
-        result = await this.streamer.send('%R8Q,5:\r\n')
-
-        off()
 
         resolve()
       })
