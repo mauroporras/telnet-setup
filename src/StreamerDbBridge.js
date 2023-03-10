ad67bde506d7d3fa2909476e54317c80940a53c0
mauroporras <167700+mauroporras@users.noreply.github.com>
2023-03-10 14:23:32 -0500

    feat: queue commands comming from DB

--- src/StreamerDbBridge.js
+++ src/StreamerDbBridge.js
@@ -1,7 +1,11 @@
+import { CommandQueue } from './helpers/CommandQueue.js'
+import { Command } from './models/Command.js'
+
 class StreamerDbBridge {
   constructor(streamer, session) {
     this.streamer = streamer
     this.session = session
+    this.commandQueue = new CommandQueue()
   }
 
   async start() {
@@ -9,6 +13,11 @@ class StreamerDbBridge {
     await this.session.init()
 
     //const res = await this.streamer.send('uptime')
+
+    this.session.onCommandCreated((data) => {
+      const command = new Command(this.streamer, this.session, data)
+      this.commandQueue.addCommand(command)
+    })
 
     this.streamer.on('data', (data) => {
       this.session.addPoint(data)
