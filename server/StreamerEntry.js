54d703e30b25085dc3983c43a10f460908ef0f47
Nathan Barnes <nbarnes@azahner.com>
2023-09-19 11:14:22 -0500

    Revert "hello message for updates"
    This reverts commit 2a7fe3099aab021d8e692b73a9df8c78528d301b.

--- server/StreamerEntry.js
+++ server/StreamerEntry.js
@@ -4,24 +4,7 @@ import { StreamerDbBridge } from './StreamerDbBridge.js'
 
 import { Session } from './models/Session.js'
 
-const printStart = () => {
-  const consoleOptions = 'background: #ffffff; color: #6b17e8';
-  // Standard Figlet Font
-  console.log("Running Version: ", "1.0.1", '\n')
 
-
- 
-  console.log('%c                                _ _       _  ', consoleOptions);
-  console.log('%c                               | (_)     | |   ', consoleOptions);
-  console.log('%c ___ _   _ _ ____   _____ _   _| |_ _ __ | | __', consoleOptions);
-  console.log('%c/ __| | | | \'__\ \ / / _ \ | | | | | \'_ \| |/ /', consoleOptions);
-  console.log('%c\__ \ |_| | |   \ V /  __/ |_| | | | | | |   < ', consoleOptions);
-  console.log('%c|___/\__,_|_|    \_/ \___|\__, |_|_|_| |_|_|\_\\', consoleOptions);
-  console.log('%c                           __/ |               ', consoleOptions);
-  console.log('%c                           |___/ ', consoleOptions);
-
- 
-}
 
 const  StreamerEntry = async (stationMac, sessID, stationName) => {
 
@@ -59,10 +42,7 @@ const  StreamerEntry = async (stationMac, sessID, stationName) => {
   const session = new Session(sessionId)
 
   const streamerDbBridge = new StreamerDbBridge(streamer, session)
-  
-  printStart()
   console.log('session started ')
-
   
   const dataReturned = await streamerDbBridge.start()
   // console.log('dataReturned', dataReturned)
