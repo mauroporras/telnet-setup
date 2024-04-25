1906142689142d0ba7c172c9fddf100e158f3d82
Nathan Barnes <70773342+nathan-barnes@users.noreply.github.com>
2024-04-25 13:44:51 -0700

    Window Tablet Launch

--- Z_LaunchServer.bat
+++ Z_LaunchServer.bat
@@ -1,4 +1,4 @@
-cd "%UserProfile%\Documents\GitHub\telnet-setup\client"
+cd "%UserProfile%\onedrive\Documents\GitHub\telnet-setup\client"
 start yarn start
-cd "%UserProfile%\Documents\GitHub\telnet-setup"
+cd "%UserProfile%\onedrive\Documents\GitHub\telnet-setup"
 start yarn start
